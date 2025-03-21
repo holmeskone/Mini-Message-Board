0e09a1b4f08c7cc655c2a59116665764886b42f5
holmeskone <165698602+holmeskone@users.noreply.github.com>
2025-03-21 10:44:20 +0100

    Added an id to the messages array, for the route parameters I use ID and user, and find the whole message and send it to the detialedMessage template to display, I also export the indexrouter and the message. I export the message so we can push the details from the newRouter into the array

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,22 +1,31 @@
-// routes/authorRouter.js
-const { Router } = require("express");
-const indexRouter = Router();
+const express = require("express");
+const indexRouter = express.Router()
 
 const messages = [
-    {
-      text: "Hi there!",
-      user: "Amando",
-      added: new Date()
-    },
-    {
-      text: "Hello World!",
-      user: "Charles",
-      added: new Date()
-    }
-  ];
+  {
+    text: "Hi there!",
+    user: "Amando",
+    id: crypto.randomUUID(),
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    id: crypto.randomUUID(),
+    added: new Date(),
+  },
+];
 
 indexRouter.get("/", (req, res) => {
-  res.render("index", { message: messages });
+  res.render("index", { messages }); 
 });
 
-module.exports = indexRouter;
+indexRouter.get("/:user/:id",(req,res) => {
+  const {user, id} = req.params;
+  console.log("Params:", req.params);
+  const message = messages.find(m => m.id == id && m.user == user);
+  console.log(message)
+  res.render("detailMessage",{ message})
+})
+
+module.exports = { router: indexRouter, messages };
