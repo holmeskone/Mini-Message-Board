d64536a8950bf115f02db0fb4f57b194b7417c81
holmeskone <165698602+holmeskone@users.noreply.github.com>
2025-03-21 10:42:05 +0100

    Just moved around pre-existing code, and added different import of indexRouter

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require("express");
-const indexRouter = require("./routes/indexRouter")
-const path = require("node:path");
 const app = express();
+const { router: indexRouter } = require('./routes/indexRouter');
+const newRouter = require("./routes/newRouter");
+const path = require("node:path");
 
 
+// Set EJS as the template engine
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.get("/", indexRouter);
+
+// Middleware to parse form data (should be before routes)
+app.use(express.urlencoded({ extended: true }));
+
+// Use routers
+app.use("/", indexRouter); 
+app.use("/new", newRouter);
 
 const PORT = 3000;
 app.listen(PORT, () => {
