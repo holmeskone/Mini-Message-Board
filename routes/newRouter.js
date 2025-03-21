adf08f9ab833e294569a79c0761dd4df1d04f327
holmeskone <165698602+holmeskone@users.noreply.github.com>
2025-03-21 10:45:32 +0100

    Added a post that send all form info to the messages array in indexRouter.js, then we redirect to that page to display latest array

---
+++ routes/newRouter.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const newRouter = express.Router()
+const { router: indexRouter, messages } = require("./indexRouter");
+
+
+newRouter.get("/", (req,res) => {
+    res.render("form");
+});
+
+newRouter.post("/", (req, res) => {
+    console.log(req.body)
+    messages.push({ text: req.body.messageText, user: req.body.messageUser, id: crypto.randomUUID(), added: new Date() });
+    res.redirect("/");
+  });
+
+module.exports = newRouter;
