4e24c72187d6985f5e63b933e7bf4086def62302
holmeskone <165698602+holmeskone@users.noreply.github.com>
2025-03-25 10:44:49 +0100

    Import dot env and call postgres, additionally handle connection URI

---
+++ db/pool.js
@@ -0,0 +1,8 @@
+const { Pool } = require("pg");
+require('dotenv').config()
+
+
+// Again, this should be read from an environment variable
+module.exports = new Pool({
+    connectionString: `postgresql://${process.env.DB_USERNAME}:${process.env.DB_PW}@${process.env.DB_PORT}/${process.env.DB_NAME}`,
+});
