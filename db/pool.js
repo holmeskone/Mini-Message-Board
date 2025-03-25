639292ed2180c41bad2bfbc2c0eca22418275143
holmeskone <165698602+holmeskone@users.noreply.github.com>
2025-03-25 12:13:09 +0100

    Debuggin

--- db/pool.js
+++ db/pool.js
@@ -1,8 +1,21 @@
-const { Pool } = require("pg");
-require('dotenv').config()
+const { Pool } = require('pg');
 
+console.log('Environment Variables:');
+console.log('PGUSER:', process.env.PGUSER);
+console.log('PGHOST:', process.env.PGHOST);
+console.log('PGDATABASE:', process.env.PGDATABASE);
+console.log('PGPASSWORD:', process.env.PGPASSWORD ? '***MASKED***' : 'UNDEFINED');
+console.log('PGPORT:', process.env.PGPORT);
 
-// Again, this should be read from an environment variable
-module.exports = new Pool({
-    connectionString: `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}`,
+const pool = new Pool({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+  ssl: { 
+    rejectUnauthorized: false 
+  }
 });
+
+module.exports = pool;
