ef30f1c6cfcd8aec6e00d571f115af5fee6a0184
holmeskone <165698602+holmeskone@users.noreply.github.com>
2025-03-25 12:05:45 +0100

    Edit connectionString according to deployment

--- db/pool.js
+++ db/pool.js
@@ -4,5 +4,5 @@ require('dotenv').config()
 
 // Again, this should be read from an environment variable
 module.exports = new Pool({
-    connectionString: `postgresql://${process.env.DB_USERNAME}:${process.env.DB_PW}@${process.env.DB_PORT}/${process.env.DB_NAME}`,
+    connectionString: `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}`,
 });
