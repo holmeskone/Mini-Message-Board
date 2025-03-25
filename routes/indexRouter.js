552fa23e2ab49348d9f6a499050066fe42d8de77
holmeskone <165698602+holmeskone@users.noreply.github.com>
2025-03-25 10:46:26 +0100

    Remove previous array and insert simple get request to controller

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,33 +1,31 @@
-const express = require("express");
-const crypto = require('crypto');
-const indexRouter = express.Router()
+const { Router } = require("express");
+const indexRouter = Router();
+const messageController = require("../controllers/messageController");
 
 
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    id: crypto.randomUUID(),
-    added: new Date(),
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    id: crypto.randomUUID(),
-    added: new Date(),
-  },
-];
+// const messages = [
+//   {
+//     text: "Hi there!",
+//     user: "Amando",
+//     id: crypto.randomUUID(),
+//     added: new Date(),
+//   },
+//   {
+//     text: "Hello World!",
+//     user: "Charles",
+//     id: crypto.randomUUID(),
+//     added: new Date(),
+//   },
+// ];
 
-indexRouter.get("/", (req, res) => {
-  res.render("index", { messages }); 
-});
+indexRouter.get("/", messageController.messagesGet);
 
-indexRouter.get("/:user/:id",(req,res) => {
-  const {user, id} = req.params;
-  console.log("Params:", req.params);
-  const message = messages.find(m => m.id == id && m.user == user);
-  console.log(message)
-  res.render("detailMessage",{ message})
-})
+// indexRouter.get("/:user/:id",(req,res) => {
+//   const {user, id} = req.params;
+//   console.log("Params:", req.params);
+//   const message = messages.find(m => m.id == id && m.user == user);
+//   console.log(message)
+//   res.render("detailMessage",{ message})
+// })
 
-module.exports = { router: indexRouter, messages };
+module.exports = indexRouter;
