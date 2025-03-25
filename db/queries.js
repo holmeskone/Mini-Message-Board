4701a0128efdd9300ff470dcd5623e70fcfa6b12
holmeskone <165698602+holmeskone@users.noreply.github.com>
2025-03-25 10:45:45 +0100

    Add two functionalitys get all messages for index and post message for create section

---
+++ db/queries.js
@@ -0,0 +1,18 @@
+const pool = require("./pool");
+
+async function getAllMessages() {
+  const { rows } = await pool.query("SELECT * FROM messages");
+  return rows;
+}
+
+async function insertMessage(username,text) {
+    await pool.query(
+        "INSERT INTO messages (username, text, added) VALUES ($1, $2, NOW())",
+        [username, text]
+      );
+      }
+
+module.exports = {
+    getAllMessages,
+    insertMessage
+};
